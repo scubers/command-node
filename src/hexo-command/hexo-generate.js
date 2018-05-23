c00e3d1a5d978de1cf55b390049a36f9c1afd340
J <jr-wong@qq.com>
2018-05-23 17:44:15 +0800

    1

--- src/hexo-command/hexo-generate.js
+++ src/hexo-command/hexo-generate.js
@@ -3,29 +3,31 @@
 var child_process = require('child_process')
 
 
-function Hexo() {
-
+class Hexo {
+    constructor() {
+    }
+    start(server) {
+        this.generate(server);
+    }
+    generate(server) {
+        server.get("/hexo/generate", (req, resp) => {
+            var command = "cd /data/hexo-server-nodejs; sh generate-hexo.sh";
+            child_process.exec(command, (err, out, errStr) => {
+                console.log(err);
+                console.log(out);
+                console.log(errStr);
+                if (err != null) {
+                    resp.send(err);
+                }
+                else {
+                    resp.send({ "result": 0 });
+                }
+            });
+        });
+    }
 }
 
-Hexo.prototype.start = function (server) {
-    this.generate(server)
-}
 
-Hexo.prototype.generate = function(server) {
-    server.post("/hexo/generate", (req, resp) => {
-        var command = "cd /data/hexo-server-nodejs; sh generate-hexo.sh"
-        child_process.exec(command, (err, out, errStr) => {
-          console.log(err)
-          console.log(out)
-          console.log(errStr)
-          if (err != null) {
-              resp.send(err)
-          } else {
-              resp.send({"result": 0})
-          }
-        })
-      })
-}
 
 module.exports = {
     Hexo: Hexo
